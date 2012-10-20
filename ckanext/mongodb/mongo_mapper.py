4d4cb95d425ef05cf3755c81a17bf10100985114
= <alexbyrnes@gmail.com>
2012-10-20 11:55:57 -0500

    Delete, config file, new collection name

--- ckanext/mongodb/mongo_mapper.py
+++ ckanext/mongodb/mongo_mapper.py
@@ -1,16 +1,22 @@
 import ckan.plugins as plugins
 import ckan.model.package
+from pylons import config
 
 from pymongo import Connection
+
 
 class MongoMapper(plugins.SingletonPlugin):
     plugins.implements(plugins.IMapper, inherit=True)
     plugins.implements(plugins.IPackageController, inherit=True)
 
+    DEFAULT_CONNECTION = 'mongodb://localhost:27017'
+    DATABASE = 'ckan_db'
+    COLLECTION = 'datasets'
+
 
     def before_update(self, mapper, connection, instance):
-        connection = Connection('localhost', 27017)
-        collection = connection['ckan_db']['packages']
+        mongocon = self.connect()
+        collection = mongocon[self.DATABASE][self.COLLECTION]
         
         package = instance.as_dict()
 
@@ -19,20 +25,36 @@ class MongoMapper(plugins.SingletonPlugin):
             package['_id'] = package['id']
             collection.save(package)
         
-        connection.disconnect()
+        mongocon.disconnect()
 
 
     def before_insert(self, mapper, connection, instance):
         return self.before_update(mapper, connection, instance)
 
+    def before_delete(self, mapper, connection, instance):
+        mongocon = self.connect()
+        collection = mongocon[self.DATABASE][self.COLLECTION]
+        
+        package = instance.as_dict()
+
+        #Filter out non-package updates
+        if isinstance(instance, ckan.model.package.Package):
+            collection.remove({'_id':package['id']})
+
+        mongocon.disconnect()
+
+
     #IPackageController
     def before_view(self, pkg_dict):
-
-        connection = Connection('localhost', 27017)
-        collection = connection['ckan_db']['packages']
+        mongocon = self.connect()
+        collection = mongocon[self.DATABASE][self.COLLECTION]
 
         results = collection.find_one({'_id': pkg_dict['id']})
 
-        connection.disconnect()
+        mongocon.disconnect()
+
         return results
 
+    def connect(self):
+        conn_string = config.get('mongodb.connection_string',self.DEFAULT_CONNECTION)
+        return Connection(conn_string)
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1'
+version = '0.2'
 
 setup(
 	name='ckanext-mongodb',
@@ -10,10 +10,10 @@ setup(
 	long_description="""\
 	""",
 	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-	keywords='',
+	keywords='mongodb, ckan',
 	author='Alex Byrnes',
 	author_email='alexbyrnes@gmail.com',
-	url='',
+	url='https://github.com/alexbyrnes/ckanext-mongodb',
 	license='',
 	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
 	namespace_packages=['ckanext', 'ckanext.mongodb'],
@@ -26,8 +26,6 @@ setup(
 	entry_points=\
 	"""
         [ckan.plugins]
-	# Add plugins here, eg
-	# myplugin=ckanext.mongodb:PluginClass
     mongodb=ckanext.mongodb.mongo_mapper:MongoMapper
     """,
 )
